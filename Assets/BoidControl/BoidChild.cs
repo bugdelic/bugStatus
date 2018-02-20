3d952c3e7f0bb368aa0e57e6b7343d9717cb7abf
hirotakaster <hirotakaster@gmail.com>
2018-02-20 12:38:42 +0900

    複数ボスの群体対応

---
+++ Assets/BoidControl/BoidChild.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace AssemblyCSharp
+{
+	public class Boid
+	{
+		// 漢字の辺、つくり、ひらがな、カタカナで個体の挙動を
+		// 変えたりする時に利用する
+		public enum Status {
+			HEN,
+			TUKURI,
+			HIRAGANA,
+			KATAKANA
+		};			
+		public Status status;
+
+		// とりあえずGameObject
+		public GameObject body;
+
+		// 自身が個体の時にターゲットとしているBoss
+		public Boid targetBoss;
+	}
+}
+
