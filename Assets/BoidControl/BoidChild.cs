c53aae6a7435492b8b271c0c3dfaef7edcdb473b
hirotakaster <hirotakaster@gmail.com>
2018-02-20 14:03:35 +0900

    生成・消滅時間を追記。TODO: Stateのもたせ方。

--- Assets/BoidControl/BoidChild.cs
+++ Assets/BoidControl/BoidChild.cs
@@ -5,8 +5,13 @@ namespace AssemblyCSharp
 {
 	public class Boid
 	{
-		// 漢字の辺、つくり、ひらがな、カタカナで個体の挙動を
-		// 変えたりする時に利用する
+		// 漢字の辺、つくり、ひらがな、カタカナで個体の挙動を変えたりする時に利用する
+		// TODO:特定のenum定義じゃなく、利便性のためにナンバリングしたstateにした方が良いかも。
+		//      public int status = 0; // 0:default
+		//                             // 1以降の数字はBoidを制御する側で任意に定義して
+		//                             // state machineとして利用できるように...みたいな。
+		//                             // このBoidはstate(性格)を持たない個としてのみ定義
+		//                             // した方が利便性が上がりそう。
 		public enum Status {
 			HEN,
 			TUKURI,
@@ -15,11 +20,21 @@ namespace AssemblyCSharp
 		};			
 		public Status status;
 
+
 		// とりあえずGameObject
 		public GameObject body;
 
 		// 自身が個体の時にターゲットとしているBoss
 		public Boid targetBoss;
+
+		// 自身が消滅するタイムアウト
+		public float extinctionTimeOut;
+
+		// 自身が生成された時間
+		private float createTime;
+
+		// 加速度(個々に加速度を保持)
+		private Vector3 acceleration;
 	}
 }
 
