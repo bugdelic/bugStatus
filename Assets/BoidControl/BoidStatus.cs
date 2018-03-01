57f6a58f44c363d13332aea2bcc95ed20eb0972b
hirotakaster <hirotakaster@gmail.com>
2018-03-01 14:24:02 +0900

    boidsの生存時間を追加

---
+++ Assets/BoidControl/BoidStatus.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoidStatus
+{
+	// Boid毎のステータス(仮で何かに使うかも)
+	public int status;
+
+	// 自身が消滅するタイムアウト
+	public float lifetime;
+
+	// 自身が生成された時間
+	public float createTime;
+
+	// 加速度(個々に加速度を保持するか...)
+	public float acceleration;
+
+	public BoidStatus () {
+		createTime = Time.time;
+	}
+}
--- Assets/BoidControl/BoidsController.cs
+++ Assets/BoidControl/BoidsController.cs
@@ -8,16 +8,6 @@ using System.Collections;
 public class BoidsController : MonoBehaviour
 {
 	/// <summary>
-	/// 群れの個体数を指定します。
-	/// </summary>
-	public int MaxChild = 50;
-
-	/// <summary>
-	/// ボスの個体数を指定します。
-	/// </summary>
-	public int MaxBoss = 4;
-
-	/// <summary>
 	/// 群れの乱れ具合を指定します。
 	/// 大きいほど各個体の個性が反映されまとまりません。
 	/// 最小値は 0, 最大値は 1 です。
@@ -27,11 +17,19 @@ public class BoidsController : MonoBehaviour
 	/// <summary>
 	/// 各個体の距離を指定します。大きいほど各個体が距離を取ります。
 	/// </summary>
-	public float Distance = 2f;
+	public float Distance = 20.0f;
+
+	/// <summary>
+	/// 各個体の生存期間の上限/下限を設定します。
+	/// 個体生成時にRandom(min, max)で当てちゃいます。
+	/// </summary>
+	public float LifetimeMax = 120.0f;
+	public float LifetimeMin = 30.0f;
+
 
 	/// <summary>
 	/// 群れのコントローラです。ボスと名付けます。
-	/// 群れはボスを基準に動きます。複数ボス
+	/// 群れはボスを基準に動きます。複数ボスもOKです。
 	/// </summary>
 	private List<GameObject> boidsBosses = new List<GameObject> ();
 
@@ -41,20 +39,14 @@ public class BoidsController : MonoBehaviour
 	private List<GameObject> boidsChildren = new List<GameObject>();
 
 	/// <summary>
+	/// 個体ごとのステータス。
+	/// </summary>
+	Dictionary<GameObject, BoidStatus> boidStatusList = new Dictionary<GameObject, BoidStatus>(); 
+
+	/// <summary>
 	///  子を追加する時のロック用
 	/// </summary>
 	UnityEngine.Object sync = new UnityEngine.Object();
-
-	/// <summary>
-	/// 個体用のモデル
-	/// </summary>
-	//public GameObject BoidsChildModel;
-
-	/// <summary>
-	/// ボス用のモデル
-	/// </summary>
-	//public GameObject BoidsBossModel;
-
 
 	public BoidsController ()
 	{
@@ -63,20 +55,28 @@ public class BoidsController : MonoBehaviour
 	// 群へ子の追加
 	public void addChild(GameObject child) {
 		lock (sync) {
+			BoidStatus boidStatus = new BoidStatus ();
+			boidStatus.lifetime = UnityEngine.Random.Range (LifetimeMin, LifetimeMax);
 			this.boidsChildren.Add (child);
+			this.boidStatusList.Add(child, boidStatus);
 		}
 	}
 
 	// 群へボスの追加
 	public void addBoss(GameObject boss) {
 		lock (sync) {
+			BoidStatus boidStatus = new BoidStatus ();
+			boidStatus.lifetime = UnityEngine.Random.Range (LifetimeMin, LifetimeMax);
 			this.boidsBosses.Add (boss);
+			this.boidStatusList.Add(boss, boidStatus);
 		}
 	}
 
 	// 群からGameObject(子/ボス)を削除
 	public void destroyBoid(GameObject boid) {
 		lock (sync) {
+			this.boidStatusList.Remove (boid);
+
 			if (boidsChildren.Contains (boid)) boidsChildren.Remove (boid);
 			if (boidsBosses.Contains (boid)) boidsBosses.Remove (boid);
 			Destroy (boid);
@@ -92,41 +92,37 @@ public class BoidsController : MonoBehaviour
 		return false;
 	}
 
+	// 生存時間を過ぎたものは除去
+	private void checkBoidsLifetime() {
+		lock (sync) {
+			foreach (GameObject boid in this.boidsChildren) {
+				if (this.boidStatusList [boid].lifetime + this.boidStatusList [boid].createTime < Time.time) {
+					boidsChildren.Remove (boid);
+					boidStatusList.Remove (boid);
+					Destroy (boid);
+				}
+			}
+
+			foreach (GameObject boid in this.boidsBosses) {
+				if (this.boidStatusList [boid].lifetime + this.boidStatusList [boid].createTime < Time.time) {
+					boidsChildren.Remove (boid);
+					boidStatusList.Remove (boid);
+					Destroy (boid);
+				}
+			}
+		}
+	}
+
 	// Use this for initialization
 	void Start () {
-		/*
-		// 仮で子供を生成して適当な位置に置きます。
-		for (int i = 0; i < this.MaxChild; i++) {
-
-			GameObject child = GameObject.Instantiate (BoidsChildModel) as GameObject;
-			child.AddComponent<Boid>();
-			child.transform.parent = transform;
-			child.name = "BoidsChild" + i;
-			child.GetComponent<Renderer> ().enabled = true;
-			child.transform.position =
-				new Vector3(UnityEngine.Random.Range(-50f, 50f),
-					this.BoidsChildModel.transform.position.y,
-				UnityEngine.Random.Range(-50f, 50f));
-			this.boidsChildren.Add (child);
-		}
-
-		// 仮でボスを適当個数用意します
-		for (int i = 0; i < this.MaxBoss; i++) {
-			GameObject boss = GameObject.Instantiate (BoidsBossModel) as GameObject;
-			boss.transform.parent = transform;
-			boss.name = "BoidsBoss" + i;
-			boss.GetComponent<Renderer> ().enabled = true;
-			boss.transform.position =
-				new Vector3(UnityEngine.Random.Range(-30f, 30f),
-					this.BoidsBossModel.transform.position.y,
-					UnityEngine.Random.Range(-30f, 30f));
-			this.boidsBosses.Add (boss);
-		}
-		*/
 	}
 
 	// Update is called once per frame
 	void Update () {
+		
+		// 生存期間チェック
+		checkBoidsLifetime ();
+
 		lock (sync) {
 			// ボスが居る時は出来るだけボスに寄ろうとする
 			Dictionary<GameObject, GameObject> childBossMapper = new Dictionary<GameObject, GameObject>(); 
@@ -151,7 +147,7 @@ public class BoidsController : MonoBehaviour
 					// 係数が 1 のとき、完全に中央へ向かわず、0 とき、完全に中央へ向かいます。
 					Vector3 dirToCenter = (targetBoss.transform.position - child.transform.position).normalized;
 					Vector3 direction = (child.GetComponent<Rigidbody> ().velocity.normalized *
-					                    this.Turbulence + dirToCenter * (1 - this.Turbulence)).normalized;
+						this.Turbulence + dirToCenter * (1 - this.Turbulence)).normalized;
 
 					// 個体によって速さを変えます(必要なら方向も)。
 					// 別途制御用パラメータを実装して個性を表現しても良いでしょう。
@@ -163,7 +159,7 @@ public class BoidsController : MonoBehaviour
 					child.GetComponent<Rigidbody> ().velocity = direction;
 				}
 
-			// ボスがだれも居ない時、[特性-1 : 各個体は群れの中央に寄ろうとする]
+				// ボスがだれも居ない時、[特性-1 : 各個体は群れの中央に寄ろうとする]
 			} else {
 				Vector3 center = Vector3.zero;
 				foreach (GameObject child in this.boidsChildren) {
@@ -174,7 +170,7 @@ public class BoidsController : MonoBehaviour
 				foreach (GameObject child in this.boidsChildren) {
 					Vector3 dirToCenter = (center - child.transform.position).normalized;
 					Vector3 direction = (child.GetComponent<Rigidbody> ().velocity.normalized * this.Turbulence
-					                   + dirToCenter * (1 - this.Turbulence)).normalized;
+						+ dirToCenter * (1 - this.Turbulence)).normalized;
 
 					direction *= UnityEngine.Random.Range (20f, 30f);
 					child.GetComponent<Rigidbody> ().velocity = direction;
@@ -190,7 +186,7 @@ public class BoidsController : MonoBehaviour
 			// Collider を利用しない場合は、
 			// 適度な距離を保てるような仕組みを用意する必要があります。
 			foreach (GameObject child_a in this.boidsChildren) {
-				
+
 				foreach (GameObject child_b in this.boidsChildren) {
 					if (child_a == child_b) {
 						continue;
@@ -209,38 +205,33 @@ public class BoidsController : MonoBehaviour
 					}
 				}
 			}
-			/*
+
 			// 各ボスに群がる個体のベクトルの平均ベクトルを求めます。
 			Dictionary<GameObject, Vector3> boidsAverageVector = new Dictionary<GameObject, Vector3>(); 
 			Dictionary<GameObject, int> boidsCountByBoss = new Dictionary<GameObject, int>(); 
-
 			// 全体の平均ベクトルを取得しておく
 			Vector3 averageVelocity = Vector3.zero;
 			foreach (GameObject child in this.boidsChildren) {
 				averageVelocity += child.GetComponent<Rigidbody> ().velocity;
 			}
 			averageVelocity /= this.boidsChildren.Count;
-
 			if (this.boidsBosses.Count > 0) {
 				// ボス毎の平均ベクトルを求めるために初期化
 				foreach (GameObject boss in this.boidsBosses) {
 					boidsAverageVector [boss] = Vector3.zero;
 					boidsCountByBoss [boss] = 0;
 				}
-
 				// 個体の平均ベクトルをボス毎に合算
 				foreach (GameObject child in this.boidsChildren) {
 					boidsAverageVector [childBossMapper[child]] += child.GetComponent<Rigidbody> ().velocity;
 					boidsCountByBoss [childBossMapper[child]] += 1;
 				}
-
 				// ボス毎に平均ベクトルを取得(ボスが個体を持っていない場合はゼロ)
 				foreach (GameObject boss in this.boidsBosses) {
 					if (boidsCountByBoss [boss] != 0)
 						boidsAverageVector [boss] /= boidsCountByBoss [boss];
 				}
 			}
-
 			// [特性-3 : 各個体は群れの平均移動ベクトルに合わせようとする]
 			// この特性-3はなくてもそれなりに動きます。
 			// Boids の群れモデルを基本的な実装で再現すると、
@@ -258,36 +249,31 @@ public class BoidsController : MonoBehaviour
 				// 1 のとき、各個体は群れ全体の移動を考慮しません。
 				child.GetComponent<Rigidbody>().velocity = child.GetComponent<Rigidbody>().velocity * this.Turbulence
 					+ (childBossMapper.ContainsKey(child) ? boidsAverageVector[childBossMapper[child]] : averageVelocity) * (1f - this.Turbulence);
-
 				// [Option] 各個体を回転させるとき。
 				// 回転の方法は表現する対象によって切り替える必要があります。
 				// まったく回転させないのが適切な場合もあるでしょう。
-
 				// Type1 : 最も簡単なものは、個体の進行方向に合わせて回転させる方法です。
 				// 群れ全体が移動しているときは一定の方向を向きますが、移動が低速になると、
 				// 各個体が衝突してバラバラの方向を向くようになり、移動時よりも統一感が損なわれます。
-				child.transform.rotation =
-					Quaternion.Slerp(child.transform.rotation,
-						Quaternion.LookRotation(child.GetComponent<Rigidbody>().velocity.normalized),
-						Time.deltaTime * 10f);
-
+				//child.transform.rotation =
+				//	Quaternion.Slerp(child.transform.rotation,
+				//		Quaternion.LookRotation(child.GetComponent<Rigidbody>().velocity.normalized),
+				//		Time.deltaTime * 10f);
 				// Type2 : 餌に群がるような群れを表現するは、目標を見るようにすれば良いです。
 				// ここでは自分が今、属してるボス中央に設定しています。
-				child.transform.LookAt((childBossMapper.ContainsKey(child) ? childBossMapper[child].transform.position : averageVelocity));
-
+				//child.transform.LookAt((childBossMapper.ContainsKey(child) ? childBossMapper[child].transform.position : averageVelocity));
 				// Type3 : 魚群のような、方向がそろうような群れは、
 				// 群れの平均移動ベクトルを利用すれば良いです。
 				// Type1 の方が生物の特性に近い挙動はしますが、魚群のようなものを表現する場合には、
 				// Type3 のような極端な表現の方がらしく見えます。
 				// 全体の移動量が少ないときも各個体が同じ方向を向くので、
 				// 静止時用の特別な動きの実装がなくてもそれらしく見えます。
-				child.transform.rotation =
-				    Quaternion.Slerp(child.transform.rotation,
-						Quaternion.LookRotation(
-							(childBossMapper.ContainsKey(child) ? boidsAverageVector[childBossMapper[child]].normalized : averageVelocity.normalized)),
-							Time.deltaTime * 3f);
+				//child.transform.rotation =
+				//	Quaternion.Slerp(child.transform.rotation,
+				//		Quaternion.LookRotation(
+				//			(childBossMapper.ContainsKey(child) ? boidsAverageVector[childBossMapper[child]].normalized : averageVelocity.normalized)),
+				//		Time.deltaTime * 3f);
 			}
-			*/
 		}
 	}
 }
